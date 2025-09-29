8cfbb834d30dc3ba29f14a9c74a0e8c9502da213
Deepakram007 <deepakramsekar@gmail.com>
2025-09-29 15:42:28 +0000

    map changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ function App() {
   return (
 
       <div>
-        <TravelDashboard></TravelDashboard>
-        {/* <MapboxComponent/> */}
+        {/* <TravelDashboard></TravelDashboard> */}
+        <MapboxComponent/>
         </div>
   )
 }
